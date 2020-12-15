7f5ddf81382851e57fbccfe730c6593afba28480
floter1 <floter1@gmail.com>
2020-12-15 11:54:39 +0800

    C:/Box Sync/Shared/software/PortableGit/var/www/wsgi in pythonanywhere remote

---
+++ floter1_pythonanywhere_com_wsgi.py
@@ -0,0 +1,25 @@
+# This file contains the WSGI configuration required to serve up your
+# web application at http://floter1.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+# The below has been auto-generated for your Django project
+
+import os
+import sys
+
+# add your project directory to the sys.path
+project_home = '/home/floter1/fscratch'
+if project_home not in sys.path:
+    sys.path.insert(0, project_home)
+
+# set environment variable to tell django where your settings.py is
+# os.environ['DJANGO_SETTINGS_MODULE'] = 'fscratch.settings.dev'
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "fscratch.settings.dev")
+
+
+# serve django via WSGI
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
+
